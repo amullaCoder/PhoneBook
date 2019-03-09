569837637344d3f1189b54a608328328fae52e12
Abdulkarim Mulla <41154564+amullaCoder@users.noreply.github.com>
2019-03-09 18:22:05 +0200

    Add files via upload

---
+++ Contacts/Contacts/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Contacts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.NewBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EmailBox = new System.Windows.Forms.TextBox();
+            this.NumberBox = new System.Windows.Forms.TextBox();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EmailDisplayBox = new System.Windows.Forms.TextBox();
+            this.NameDisplayBox = new System.Windows.Forms.TextBox();
+            this.DisplayBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NumberDisplayBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.SaveBtn);
+            this.panel1.Controls.Add(this.CancelBtn);
+            this.panel1.Controls.Add(this.NewBtn);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.EmailBox);
+            this.panel1.Controls.Add(this.NumberBox);
+            this.panel1.Controls.Add(this.NameBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(595, 234);
+            this.panel1.TabIndex = 0;
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Enabled = false;
+            this.SaveBtn.Location = new System.Drawing.Point(229, 178);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(75, 23);
+            this.SaveBtn.TabIndex = 11;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Location = new System.Drawing.Point(386, 113);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.CancelBtn.TabIndex = 10;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // NewBtn
+            // 
+            this.NewBtn.Location = new System.Drawing.Point(386, 78);
+            this.NewBtn.Name = "NewBtn";
+            this.NewBtn.Size = new System.Drawing.Size(75, 23);
+            this.NewBtn.TabIndex = 8;
+            this.NewBtn.Text = "New";
+            this.NewBtn.UseVisualStyleBackColor = true;
+            this.NewBtn.Click += new System.EventHandler(this.NewBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(30, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Phone Number:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(92, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(92, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name:";
+            // 
+            // EmailBox
+            // 
+            this.EmailBox.Enabled = false;
+            this.EmailBox.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailBox.Location = new System.Drawing.Point(152, 143);
+            this.EmailBox.Name = "EmailBox";
+            this.EmailBox.Size = new System.Drawing.Size(228, 29);
+            this.EmailBox.TabIndex = 3;
+            // 
+            // NumberBox
+            // 
+            this.NumberBox.Enabled = false;
+            this.NumberBox.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumberBox.Location = new System.Drawing.Point(152, 108);
+            this.NumberBox.Name = "NumberBox";
+            this.NumberBox.Size = new System.Drawing.Size(228, 29);
+            this.NumberBox.TabIndex = 2;
+            // 
+            // NameBox
+            // 
+            this.NameBox.Enabled = false;
+            this.NameBox.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameBox.Location = new System.Drawing.Point(152, 73);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(228, 29);
+            this.NameBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(142, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Contacts";
+            // 
+            // EmailDisplayBox
+            // 
+            this.EmailDisplayBox.Enabled = false;
+            this.EmailDisplayBox.Location = new System.Drawing.Point(370, 294);
+            this.EmailDisplayBox.Multiline = true;
+            this.EmailDisplayBox.Name = "EmailDisplayBox";
+            this.EmailDisplayBox.Size = new System.Drawing.Size(235, 161);
+            this.EmailDisplayBox.TabIndex = 13;
+            // 
+            // NameDisplayBox
+            // 
+            this.NameDisplayBox.Enabled = false;
+            this.NameDisplayBox.Location = new System.Drawing.Point(12, 294);
+            this.NameDisplayBox.Multiline = true;
+            this.NameDisplayBox.Name = "NameDisplayBox";
+            this.NameDisplayBox.Size = new System.Drawing.Size(185, 161);
+            this.NameDisplayBox.TabIndex = 1;
+            // 
+            // DisplayBtn
+            // 
+            this.DisplayBtn.Location = new System.Drawing.Point(232, 461);
+            this.DisplayBtn.Name = "DisplayBtn";
+            this.DisplayBtn.Size = new System.Drawing.Size(142, 34);
+            this.DisplayBtn.TabIndex = 9;
+            this.DisplayBtn.Text = "Display All Contacts";
+            this.DisplayBtn.UseVisualStyleBackColor = true;
+            this.DisplayBtn.Click += new System.EventHandler(this.DisplayBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Perpetua Titling MT", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 34);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Contacts:";
+            // 
+            // NumberDisplayBox
+            // 
+            this.NumberDisplayBox.Enabled = false;
+            this.NumberDisplayBox.Location = new System.Drawing.Point(194, 294);
+            this.NumberDisplayBox.Multiline = true;
+            this.NumberDisplayBox.Name = "NumberDisplayBox";
+            this.NumberDisplayBox.Size = new System.Drawing.Size(180, 161);
+            this.NumberDisplayBox.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 508);
+            this.Controls.Add(this.NumberDisplayBox);
+            this.Controls.Add(this.EmailDisplayBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DisplayBtn);
+            this.Controls.Add(this.NameDisplayBox);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Contacts";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Button NewBtn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox EmailBox;
+        private System.Windows.Forms.TextBox NumberBox;
+        private System.Windows.Forms.TextBox NameDisplayBox;
+        private System.Windows.Forms.Button DisplayBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox EmailDisplayBox;
+        private System.Windows.Forms.TextBox NumberDisplayBox;
+    }
+}
+
---
+++ Contacts/Contacts/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contacts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Path to the text files.
+        String namePath = @"C:\Users\Abdulkarim Mulla\Desktop\Programming\C#\Contacts\Names.txt";
+        String numberPath = @"C:\Users\Abdulkarim Mulla\Desktop\Programming\C#\Contacts\Numbers.txt";
+        String emailPath = @"C:\Users\Abdulkarim Mulla\Desktop\Programming\C#\Contacts\Emails.txt";
+
+        //Tab for making space.
+        String tab = "\t";
+
+        private void NewBtn_Click(object sender, EventArgs e)
+        {
+            //Adds a new contact to the phonebook and Enables the buttons.
+            panel1.Enabled = true;
+            EnableTextBox();
+            NameBox.Focus();
+
+        }
+        
+
+        //Cancel button for disabling the text boxes.
+        private void CancelBtn_Click(object sender, EventArgs e)
+        {
+            DisableTextBox();   
+        }
+
+        //Method for disabling textboxes
+        private  void DisableTextBox()
+        {
+            NameBox.Enabled = false;
+            NumberBox.Enabled = false;
+            EmailBox.Enabled = false;
+            SaveBtn.Enabled = false;
+        }
+        private void ClearContent()
+        {
+            NameBox.Clear();
+            NumberBox.Clear();
+            EmailBox.Clear();
+        }
+
+        //Method for Enabling textboxes
+        private void EnableTextBox()
+        {
+            NameBox.Enabled = true;
+            NumberBox.Enabled = true;
+            EmailBox.Enabled = true;
+            SaveBtn.Enabled = true;
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            if (NumberBox.Text == "" && EmailBox.Text == "")
+            {
+                MessageBox.Show("Email or Phone Number cannot be empty!", "ALERT!");
+            }
+            else if (NameBox.Text == "")
+            {
+                MessageBox.Show("Name cannot be empty!", "ALERT!");
+            }
+            else if (NumberBox.Text == "" && EmailBox.Text != "")
+            {
+                System.IO.File.AppendAllText(namePath, NameBox.Text + "\r\n");
+                System.IO.File.AppendAllText(numberPath, tab + "\r\n");
+                System.IO.File.AppendAllText(emailPath, EmailBox.Text + "\r\n");
+                ClearContent();
+            }
+            else if (NumberBox.Text != "" && EmailBox.Text == "")
+            {
+                System.IO.File.AppendAllText(namePath, NameBox.Text + "\r\n");
+                System.IO.File.AppendAllText(numberPath, NumberBox.Text + "\r\n");
+                System.IO.File.AppendAllText(emailPath, tab + "\r\n");
+                ClearContent();
+            }
+            else
+            {
+                System.IO.File.AppendAllText(namePath, NameBox.Text + "\r\n");
+                System.IO.File.AppendAllText(numberPath, NumberBox.Text + "\r\n");
+                System.IO.File.AppendAllText(emailPath, EmailBox.Text + "\r\n");
+                ClearContent();
+            }
+        }
+
+        private void DisplayBtn_Click(object sender, EventArgs e)
+        {
+
+            String names = System.IO.File.ReadAllText(namePath, Encoding.UTF8);
+            String numbers = System.IO.File.ReadAllText(numberPath, Encoding.UTF8);
+            String email = System.IO.File.ReadAllText(emailPath, Encoding.UTF8);
+
+            NameDisplayBox.Text = names;
+            NumberDisplayBox.Text = numbers;
+            EmailDisplayBox.Text = email;
+        }
+
+    }
+}
